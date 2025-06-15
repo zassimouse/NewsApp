e1799af9a43307286adb94f7fde78e6a6803b895
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2025-06-15 16:44:12 +0300

    feat: add supplementary views, category sorting

--- NewsApp/ContentView.swift
+++ NewsApp/ContentView.swift
@@ -14,28 +14,37 @@ struct ContentView: View {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.labelPrimary]
     }
     
+    @StateObject private var viewModel = NewsViewModel()
+    
     @State private var selectedOption = 0
     private let options = ["All", "Favorites", "Blocked"]
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 10) {
-                VStack {
-                    Picker("Options", selection: $selectedOption) {
-                        ForEach(0..<options.count, id: \.self) { index in
-                            Text(options[index])
-                                .tag(index)
+            LazyVStack(spacing: 10) {
+                Picker("Options", selection: $viewModel.selectedOption) {
+                    ForEach(0..<viewModel.options.count, id: \.self) { index in
+                        Text(viewModel.options[index])
+                            .tag(index)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+
+                ForEach(Array(viewModel.filteredArticles.enumerated()), id: \.element.id) { index, article in
+                    VStack(spacing: 10) {
+                        ArticleCell(article: article, viewModel: viewModel)
+                            .onTapGesture {
+                                if let url = URL(string: article.URLString) {
+                                    UIApplication.shared.open(url)
+                                }
+                            }
+                        
+                        if viewModel.selectedOption == 0 && (index + 1) % 2 == 0 && index != viewModel.filteredArticles.count - 1 {
+                            SupplementaryCell()
                         }
                     }
-                    .pickerStyle(SegmentedPickerStyle())
                 }
-                               
-                SupplementaryCell()
                 
-                ForEach(1...10, id: \.self) { index in
-                    ArticleCell()
-                }
-
                 
             }
             .padding(.horizontal)
