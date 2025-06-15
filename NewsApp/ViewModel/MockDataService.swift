09514c1ca3cc187cf4b05e332defefa3c2b47299
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2025-06-15 16:40:43 +0300

    feat: add Arcticle model

---
+++ NewsApp/ViewModel/MockDataService.swift
@@ -0,0 +1,8 @@
+//
+//  MockDataService.swift
+//  NewsApp
+//
+//  Created by Denis Haritonenko on 14.06.25.
+//
+
+import Foundation
---
+++ NewsApp/ViewModel/NewsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewsViewModel.swift
+//  NewsApp
+//
+//  Created by Denis Haritonenko on 14.06.25.
+//
+
+import Foundation
