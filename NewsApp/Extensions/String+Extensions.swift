b82500b3ebdba14e1d5017abba9c35f3d350d12e
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2025-06-16 19:34:26 +0300

    feat: format date string

--- NewsApp/Extensions/String+Extensions.swift
+++ NewsApp/Extensions/String+Extensions.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension String {
+    func formattedDate(
+        from inputFormat: String = "yyyy-MM-dd",
+        to outputFormat: String = "MMM d, yyyy",
+        locale: Locale = .current
+    ) -> String? {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = inputFormat
+        
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = outputFormat
+        outputFormatter.locale = locale
+        
+        guard let date = inputFormatter.date(from: self) else { return nil }
+        return outputFormatter.string(from: date)
+    }
+}
